a57924966d5ce0d0555d62b0f0ee6f2c594b214c
Cubik65536 <cubik65536@outlook.com>
2020-12-29 10:20:36 -0500

    运算符按键和后面的功能写完一半了
    Signed-off-by: Cubik65536 <cubik65536@outlook.com>

--- src/top/cubik65536/java/JavaFXCalculator/core/Controller.java
+++ src/top/cubik65536/java/JavaFXCalculator/core/Controller.java
@@ -1,12 +1,13 @@
 package top.cubik65536.java.JavaFXCalculator.core;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.fxml.FXML;
 
 public class Controller {
     // 初始化变量
+    boolean isCalculationSymbolEntered = false; // 运算符是否已输入
     boolean isFirstNum = true; // 正在输入的是否为第一个要运算的数，初始化为true
     String firstNumString = ""; // 第一个要运算的数
     String secondNumString = ""; // 第二个要运算的数
@@ -121,15 +122,41 @@ public class Controller {
         System.out.println("button2Clicked");
         newButtonClicked("2");
     }
+
     @FXML
     protected void button3Clicked(ActionEvent event) {
         System.out.println("button3Clicked");
         newButtonClicked("3");
     }
+
     @FXML
     protected void button0Clicked(ActionEvent event) {
         System.out.println("button0Clicked");
         newButtonClicked("0");
+    }
+
+    @FXML
+    protected void buttonPlusClicked(ActionEvent event) {
+        System.out.println("buttonPlusClicked");
+        calculateButtonClicked((byte) 1, "+");
+    }
+
+    @FXML
+    protected void buttonMinusClicked(ActionEvent event) {
+        System.out.println("buttonMinusClicked");
+        calculateButtonClicked((byte) 2, "-");
+    }
+
+    @FXML
+    protected void buttonMultiplyClicked(ActionEvent event) {
+        System.out.println("buttonMultiplyClicked");
+        calculateButtonClicked((byte) 3, "×");
+    }
+
+    @FXML
+    protected void buttonDivisionClicked(ActionEvent event) {
+        System.out.println("buttonDivisionClicked");
+        calculateButtonClicked((byte) 4, "÷");
     }
 
     /**
@@ -147,7 +174,7 @@ public class Controller {
     private void newButtonClicked(String numString) {
         // 检测现在正在输入的是第一个还是第二个运算值
         if (isFirstNum) {
-            // 是
+            // 是，向第一个值输入
             // 检测已有数字位数，超出限定位数24就不让再输入了
             // 换言之，如果已有24位就不再允许输入了
             if (firstNumString.length() < 24) {
@@ -160,7 +187,7 @@ public class Controller {
                 refreshDisplayContent();
             }
         } else {
-            // 否
+            // 否，向第二个值输入
             // 检测已有数字位数，超出限定位数24就不让再输入了
             // 换言之，如果已有24位就不再允许输入了
             if (secondNumString.length() < 24) {
@@ -177,9 +204,30 @@ public class Controller {
 
     /**
      * 计算键触发函数，将按键符号加入显示内容并更新，数字键开始向第二个运算值字符串输入内容
-     * @param calculationType 运算类型，直接由按键的onAction函数调用时传递，包含加（1）减（2）乘（3）除（4）乘方（5）开方（6），不包含二次方运算。该值不为1到6时报错并重新初始化程序
+     *
+     * @param calculationType   运算类型，直接由按键的onAction函数在调用时传递，包含加（1）减（2）乘（3）除（4）乘方（5）开方（6），不包含二次方运算。该值不为1到6时报错并重新初始化程序
+     * @param calculationSymbol 运算符符号，直接由由按键的onAction函数在调用时传递
      */
-    private void calculateButtonClicked(byte calculationType) {
-
+    private void calculateButtonClicked(byte calculationType, String calculationSymbol) {
+        // 检测calculationType是否合格
+        if (1 <= calculationType && calculationType <= 6) {
+            // 合格
+            // 判断第一个数是否已经有值（length不为0）
+            if (firstNumString.length() > 0) {
+                // 大于0（有值）
+                // 向显示内容添加运算符号并更新
+                displayingContents = displayingContents + calculationSymbol;
+                refreshDisplayContent();
+                // 开始像第二个数输入
+                isFirstNum = false;
+                return;
+            }
+            // 等于0（无值）
+            // 不执行任何操作
+            System.out.println("没有第一个运算值！不进行任何操作。");
+            return;
+        }
+        // 不合格，不执行任何操作，向控制台输出提示
+        System.out.println("calculateButtonClicked函数的calculationType值不符合要求！要求值为1到6，本次调用传入的值是：" + calculationType);
     }
 }
